3b2d7952d5dc1d5c228c5a7cb76b49a69cb1c45e
Christoph Wertz <christoph.wertz@gmail.com>
2015-05-09 20:13:43 -0600

    Minor refactoring / clarification.

--- src/edu/stanford/cs276/CosineSimilarityScorer.java
+++ src/edu/stanford/cs276/CosineSimilarityScorer.java
@@ -18,23 +18,29 @@ import java.util.Set;
 /**
  * Skeleton code for the implementation of a Cosine Similarity Scorer in Task 1.
  */
-public class CosineSimilarityScorer extends AScorer {
+public class CosineSimilarityScorer extends AScorer 
+{
 
-	public CosineSimilarityScorer(Map<String,Double> idfs) {
+	public CosineSimilarityScorer(Map<String,Double> idfs) 
+	{
 		super(idfs);
 	}
 
 	/////////////// Weights //////////////////
+	
 	double urlweight = 1;
 	double titleweight = 1;
 	double bodyweight = 1;
 	double headerweight = 1;
 	double anchorweight = 1;
 
-	double smoothingBodyLength = 500.0; // Smoothing factor when the body length is 0.
+	// Smoothing factor when the body length is 0.
+	double smoothingBodyLength = 500.0;
+	
 	//////////////////////////////////////////
 
-	public double getNetScore(Map<String, Map<String, Double>> tfs, Query q, Map<String,Double> tfQuery, Document d) {
+	public double getNetScore(Map<String, Map<String, Double>> tfs, Query q, Map<String,Double> tfQuery, Document d) 
+	{
 		double score = 0.0;
 		
 		/*
@@ -43,59 +49,63 @@ public class CosineSimilarityScorer extends AScorer {
 		for(String term: tfQuery.keySet())	
 		{	
 			score += tfQuery.get(term)*(urlweight*tfs.get("url").get(term)
-									  + titleweight*tfs.get("title").get(term)
-									  + bodyweight*tfs.get("body").get(term)
-									  + headerweight*tfs.get("header").get(term)
-									  + anchorweight*tfs.get("anchor").get(term)); 
+					  + titleweight*tfs.get("title").get(term)
+					  + bodyweight*tfs.get("body").get(term)
+					  + headerweight*tfs.get("header").get(term)
+					  + anchorweight*tfs.get("anchor").get(term)); 
 			
 		}
-		
 		
 		return score;
 	}
 
-	// Normalize the term frequencies. Note that we should give uniform normalization to all fields as discussed
-	// in the assignment handout.
-	public void normalizeTFs(Map<String,Map<String, Double>> tfs,Document d, Query q) {
+	
+	/*
+	 * Normalize the term frequencies. Note that we should give uniform normalization to all fields as discussed
+	 * in the assignment handout.
+	 */
+	public void normalizeTFs(Map<String,Map<String, Double>> tfs,Document d, Query q) 
+	{
 		/*
 		 * @//TODO : Your code here
 		 */
-		double bodyLength_inv = 1.0/((double)d.body_length + smoothingBodyLength);
+		double invBodyLength = 1.0/((double)d.body_length + smoothingBodyLength);
 		double freq; 
-		for(String term: tfs.get("url").keySet())	
+		
+		for (String term : tfs.get("url").keySet())	
 		{		
 			freq = tfs.get("url").get(term); 
 			if(freq!=0)
 			{ 			 
-				freq *= bodyLength_inv;
+				freq *= invBodyLength;
 				tfs.get("url").put(term, freq);
 			} 
 			
 			freq = tfs.get("title").get(term); 
 			if(freq!=0)
 			{ 			 
-				freq *= bodyLength_inv;
+				freq *= invBodyLength;
 				tfs.get("title").put(term, freq);
 			}
 			
 			freq = tfs.get("body").get(term); 
 			if(freq!=0)
 			{ 			 
-				freq *= bodyLength_inv;
+				freq *= invBodyLength;
 				tfs.get("body").put(term, freq);
 			}
 			
 			freq = tfs.get("header").get(term); 
 			if(freq!=0)
 			{ 			 
-				freq *= bodyLength_inv;
+				freq *= invBodyLength;
 				tfs.get("header").put(term, freq);
 			}
 			
 			freq = tfs.get("anchor").get(term); 
 			if(freq!=0)
 			{ 			 
-				freq *= bodyLength_inv;
+				freq *= invBodyLength;
 				tfs.get("anchor").put(term, freq);
 			}
 		}
@@ -103,7 +113,8 @@ public class CosineSimilarityScorer extends AScorer {
 
 
 	@Override
-	public double getSimScore(Document d, Query q) {
+	public double getSimScore(Document d, Query q) 
+	{
 		
 		Map<String,Map<String, Double>> tfs = this.getDocTermFreqs(d,q);
 		
