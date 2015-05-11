791e22ef08e74e80b5551986465a2b0e208d9ba0
Christoph Wertz <christoph.wertz@gmail.com>
2015-05-10 22:15:46 -0600

    Calculate the query vector, and the document vectors.

--- src/edu/stanford/cs276/AScorer.java
+++ src/edu/stanford/cs276/AScorer.java
@@ -27,11 +27,15 @@ public abstract class AScorer {
 	
 	// Handle the query vector
 	public Map<String,Double> getQueryFreqs(Query q) {
-		Map<String,Double> tfQuery = new HashMap<String, Double>(); // queryWord -> term frequency
+		Map<String,Double> tfQuery = newVector(q); // queryWord -> term frequency
 		
 		/*
 		 * @//TODO : Your code here
 		 */
+		for (String term : q.queryWords)
+		{
+			tfQuery.put(term,  tfQuery.get(term) + 1.0);
+		}
 		
 		return tfQuery;
 	}
@@ -59,6 +63,41 @@ public abstract class AScorer {
 		/*
 		 * @//TODO : Your code here
 		 */
+		// "url","title","body","header","anchor"
+		
+		tfs.put("url", getTFTypeVector(q, d.url.split("[^a-zA-Z0-9']")));
+		tfs.put("title", getTFTypeVector(q, d.title.split("\\s+")));
+		
+		Map<String, Double> termFreq = newVector(q);
+		for (String term : d.body_hits.keySet())
+		{
+			if (q.queryWords.contains(term))
+			{
+				termFreq.put(term, (double)d.body_hits.get(term).size());
+			}
+		}
+		tfs.put("body", termFreq);
+		
+		ArrayList<String> headers = new ArrayList<>();
+		if (null != d.headers)
+		{
+			for (String header : d.headers)
+			{
+				headers.addAll(new ArrayList<String>(Arrays.asList(header.split("\\s+"))));
+			}
+		}
+		String[] headerArray = new String[headers.size()];
+		tfs.put("header", getTFTypeVector(q, headers.toArray(headerArray)));
+		
+		termFreq = newVector(q);
+		if (null != d.anchors)
+		{
+			for (String anchor : d.anchors.keySet())
+			{
+				termFreq = mergeVectors(q, termFreq, getTFTypeVector(q, anchor.split("\\s+"), (double)d.anchors.get(anchor)));
+			}
+		}
+		tfs.put("anchor", termFreq);
 		
 	    ////////////////////////////////////////////////////////
 		
@@ -70,6 +109,60 @@ public abstract class AScorer {
 			
 		}
 		return tfs;
+	}
+	
+	
+	private Map<String, Double> newVector(Query q) 
+	{
+		Map<String, Double> vector = new HashMap<>();
+		
+		for (String term : q.queryWords) vector.put(term, 0.0);
+		
+		return vector;
+
+	}
+	
+	
+	private Map<String, Double> mergeVectors(Query q, Map<String,Double> vector1, Map<String,Double> vector2)
+	{
+		Map<String, Double> termFreq = new HashMap<>();
+		
+		for (String term : q.queryWords)
+		{
+			termFreq.put(term, vector1.get(term) + vector2.get(term));
+		}
+		
+		return termFreq;
+	}
+	
+	
+	private Map<String, Double> getTFTypeVector(Query q, String[] terms) 
+	{
+		return getTFTypeVector(q, terms, 1.0);
+	}
+
+	
+	private Map<String, Double> getTFTypeVector(Query q, String[] terms, Double factor) 
+	{
+		Map<String, Double> termFreq = newVector(q);
+				
+		for (String term : terms)
+		{
+			if (q.queryWords.contains(term))
+			{
+				termFreq.put(term, termFreq.get(term)+1.0);
+			}
+		}
+		
+		if (factor != 1.0)
+		{
+			for (String term : q.queryWords)
+			{
+				termFreq.put(term, termFreq.get(term) * factor);
+			}
+		}
+
+		return termFreq;
 	}
 
 }
